ecb3ae7781239a8ef3f2a52f8cf033de25cb4425
Manpreet Singh <prince6179@gmail.com>
2022-02-04 16:11:01 +0400

    Adding Podspec file

---
+++ PayBright.xcframework/ios-arm64/PayBright.framework/Headers/PayBright.h
@@ -0,0 +1,18 @@
+//
+//  PayBright.h
+//  PayBright
+//
+//  Created by Manpreet Singh on 27/01/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PayBright.
+FOUNDATION_EXPORT double PayBrightVersionNumber;
+
+//! Project version string for PayBright.
+FOUNDATION_EXPORT const unsigned char PayBrightVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PayBright/PublicHeader.h>
+
+
