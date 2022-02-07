beb0e86f3e6cf78319e49c7b5d315006ec236eb4
Manpreet Singh <prince6179@gmail.com>
2022-02-07 12:52:45 +0400

    Updated package specification

--- Package.swift
+++ Package.swift
@@ -10,7 +10,13 @@ let package = Package(
             name: "PayBright", 
             targets: ["PayBright"])
     ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
     targets: [
+        .target(
+            name: "PayBright"
+        ),
         .binaryTarget(
             name: "PayBright", 
             path: "PayBright.xcframework")
