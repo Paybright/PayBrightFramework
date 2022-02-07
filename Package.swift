1ac0f760191fec336ec80bf7616131ea63c1ed98
Manpreet Singh <prince6179@gmail.com>
2022-02-07 13:15:48 +0400

    Updated package specification

--- Package.swift
+++ Package.swift
@@ -3,13 +3,13 @@ import PackageDescription
 let package = Package(
     name: "PayBright",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v13)
     ],
     products: [
         .library(
             name: "PayBright", 
             targets: ["PayBright"]
-        ),
+        )
     ],
     dependencies: [],
     targets: [
@@ -18,8 +18,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "PayBright", 
-            url: "https://github.com/Paybright/PayBrightFramework/releases/tag/0.1.5/PayBright.xcframework.zip"),
+            url: "https://github.com/Paybright/PayBrightFramework/releases/tag/0.1.5/PayBright.xcframework.zip",
             checksum: "6bfe898d51a4ee6b2bcdd603b03b34c808e646b1a5f36191033b596bf61aaf9d"
-        ),
+        )
     ]
 )
