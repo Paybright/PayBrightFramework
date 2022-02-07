688cbc55bd512f6dfae03c495758f90d0210a862
Manpreet Singh <prince6179@gmail.com>
2022-02-07 13:03:49 +0400

    Updated package specification

--- Package.swift
+++ Package.swift
@@ -19,5 +19,6 @@ let package = Package(
         ),
         .binaryTarget(
             name: "PayBright", 
-            url: "https://github.com/Paybright/PayBrightFramework.git")
+            url: "https://github.com/Paybright/PayBrightFramework/releases/tag/0.1.5/PayBright.xcframework.zip"),
+            checksum: "6bfe898d51a4ee6b2bcdd603b03b34c808e646b1a5f36191033b596bf61aaf9d"
     ])
